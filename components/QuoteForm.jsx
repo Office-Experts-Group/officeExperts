e7fc4e5f17036baff97120fdda2ed99abbbf5320
Gitit2meBaby <thisworldofdans@gmail.com>
2024-12-29 15:49:09 +0700

    fixed dynamic import

--- components/QuoteForm.jsx
+++ components/QuoteForm.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import React, { useState, useRef } from "react";
+import dynamic from "next/dynamic";
 
 import styles from "../styles/contact.module.css";
 
