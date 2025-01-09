b889c3f35b2652503a50ff39fc2598e9e98bd40e
Gitit2meBaby <thisworldofdans@gmail.com>
2025-01-09 12:14:30 +0700

    logos made to client components

--- components/Logos.jsx
+++ components/Logos.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 
 import styles from "../styles/logos.module.css";
@@ -8,27 +9,44 @@ import certifiedProfessional from "../public/microsoft/certifiedProfessional.web
 import solutionsAssociate from "../public/microsoft/solutionsAssociate.webp";
 
 const Logos = () => {
+  const [style, setStyle] = useState(1);
+  const [key, setKey] = useState(0); // Add a key state
+
+  const handleClick = () => {
+    if (style === 7) {
+      setStyle(1);
+    } else {
+      setStyle(style + 1);
+    }
+    setKey((prevKey) => prevKey + 1); // Increment key to force remount
+  };
+
   return (
-    <section className={styles.logos}>
-      <Image
-        src={certifiedPartner}
-        alt="microsoft certified partner"
-        width={200}
-        height={150}
-      />
-      <Image
-        src={certifiedProfessional}
-        alt="microsoft certified professional"
-        width={200}
-        height={150}
-      />
-      <Image
-        src={solutionsAssociate}
-        alt="microsoft solutions associate"
-        width={200}
-        height={150}
-      />
-    </section>
+    <>
+      <button className={`btn ${styles.btn}`} onClick={handleClick}>
+        Change
+      </button>
+      <section key={key} className={styles[`logos${style}`]}>
+        <Image
+          src={certifiedPartner}
+          alt="microsoft certified partner"
+          width={200}
+          height={150}
+        />
+        <Image
+          src={certifiedProfessional}
+          alt="microsoft certified professional"
+          width={200}
+          height={150}
+        />
+        <Image
+          src={solutionsAssociate}
+          alt="microsoft solutions associate"
+          width={200}
+          height={150}
+        />
+      </section>
+    </>
   );
 };
 
